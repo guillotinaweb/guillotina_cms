d30cc43259a19e864bc891412e6437055736e10c
Ramon Navarro Bosch <ramon.nb@gmail.com>
2018-07-19 16:41:40 +0200

    Container is folderish

--- guillotina_cms/json/serialize_content.py
+++ guillotina_cms/json/serialize_content.py
@@ -1,5 +1,5 @@
 from guillotina import configure
-from guillotina.interfaces import IResourceSerializeToJson, IFolder
+from guillotina.interfaces import IResourceSerializeToJson, IFolder, IContainer
 from zope.interface import Interface
 from guillotina.json.serialize_content import SerializeToJson, MAX_ALLOWED
 from guillotina.interfaces import IInteraction
@@ -43,6 +43,37 @@ class DefaultJSONSummarySerializer(object):
     for_=(IFolder, ICMSLayer),
     provides=IResourceSerializeToJson)
 class SerializeFolderToJson(SerializeToJson):
+
+    @profilable
+    async def __call__(self, include=[], omit=[]):
+        result = await super(SerializeFolderToJson, self).__call__(
+            include=include, omit=omit)
+
+        security = IInteraction(self.request)
+        length = await self.context.async_len()
+
+        if length > MAX_ALLOWED or length == 0:
+            result['items'] = []
+        else:
+            result['items'] = []
+            async for ident, member in self.context.async_items(suppress_events=True):
+                if not ident.startswith('_') and bool(
+                        security.check_permission(
+                        'guillotina.AccessContent', member)):
+                    result['items'].append(
+                        await get_multi_adapter(
+                            (member, self.request),
+                            IResourceSerializeToJsonSummary)())
+        result['length'] = length
+        result['is_folderish'] = True
+        return result
+
+
+
+@configure.adapter(
+    for_=(IContainer, ICMSLayer),
+    provides=IResourceSerializeToJson)
+class SerializeContainerToJson(SerializeToJson):
 
     @profilable
     async def __call__(self, include=[], omit=[]):
