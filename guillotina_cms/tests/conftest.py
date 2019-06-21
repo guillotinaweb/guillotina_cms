35a83c16921b8e5e4ecdedd3e0770191006f0827
vangheem <vangheem@gmail.com>
2019-06-21 11:13:52 +0200

    remove li from tests

--- guillotina_cms/tests/conftest.py
+++ guillotina_cms/tests/conftest.py
@@ -9,7 +9,6 @@ images.configure(
 pytest_plugins = [
     'pytest_docker_fixtures',
     'guillotina.tests.fixtures',
-    # 'guillotina_rediscache.tests.fixtures',
-    'guillotina_linkintegrity.tests.fixtures',
+    # 'guillotina_linkintegrity.tests.fixtures',
     'guillotina_cms.tests.fixtures'
 ]
